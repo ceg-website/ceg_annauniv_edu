e3686b815e2f9bde42899cc60f343fa19ffd9841
Senthil Ramasamy <www.senthilr.com@gmail.com>
2015-11-25 11:40:39 +0530

    exception of errors handled for fetching guindytimes feed

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -3,21 +3,24 @@
 require 'rss/1.0'
 require 'rss/2.0'
 require 'open-uri'
- 
+#require 'restclient'
 module WelcomeHelper
   
  def blog_feed
     source = "http://guindytimes.com/rss?format=rss" # url or local file
     content = "" # raw content of rss feed will be loaded here
+    html = ""
+begin
     open(source) do |s| content = s.read end
     rss = RSS::Parser.parse(content, false)
-    html = ""
     rss.items.first(10).each do |i|
     html << "<tr class='save-me' ><td class='col-md-1'><img src='/assets/gt_logo.jpg' class='gt-img'></td>
               <td class='col-md-8'><span class='auth-name'>#{i.author}</span><span class='topic'><a href='#{i.link}' target = '_blank'>#{i.title}</a></span></td>
              <td class='col-md-2'><span class='glyphicon glyphicon-time' aria-hidden='true' style='padding-right:5px;''></span>#{i.pubDate.strftime("%d %b, %Y")}</td></tr>"
     end 
-    html
-  end
-
+rescue 
+   html = "<center><h5>Retriving content ... <br/>from <a href = 'http://www.guindytimes.com' target= '_blank'>GuindyTimes.com</a></h5></center>"	
+end
+  html
+end
 end
