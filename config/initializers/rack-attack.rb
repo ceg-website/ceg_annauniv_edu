f2a2735c5400d4f2120ac9114b534169fa236d34
Senthil Ramasamy <www.senthilr.com@gmail.com>
2015-11-26 01:35:04 +0530

    RackAttack config throttle

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,3 +1,12 @@
 class Rack::Attack
   # your custom configuration...
+  throttle('req/ip', :limit => 100, :period => 5.second) do |req|
+  # If the return value is truthy, the cache key for the return value
+  # is incremented and compared with the limit. In this case:
+  #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
+  #
+  # If falsy, the cache key is neither incremented nor checked.
+
+  req.ip
+  end
 end
